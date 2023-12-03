48c96bd8f2a6de3ca3ccbdcbd701d0d3c7526e8f
Hartur <hartur.kunzler@gmail.com>
2023-12-03 01:09:27 -0300

    Testa leitura de arquivos

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import "github.com/harturk/go-first/utils"
+
 func main() {
-	println("Hello, world!")
+	content, err := utils.ReadFile("data/model.txt")
+	if err != nil {
+		panic(err)
+	}
+	println(content)
 }
