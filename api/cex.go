7f184f25769cb2201af455364a11996a9fe6dad8
Hartur <hartur.kunzler@gmail.com>
2023-12-03 23:59:07 -0300

    Cria teste

--- api/cex.go
+++ api/cex.go
@@ -13,6 +13,9 @@ import (
 const apiUrl = "https://cex.io/api/ticker/%s/USD"
 
 func GetRate(currency string) (*datatypes.Rate, error) {
+	if len(currency) < 3 {
+		return nil, fmt.Errorf("currency must be 3 characters long. Got %d", len(currency))
+	}
 	upCurrency := strings.ToUpper(currency)
 	res, err := http.Get(fmt.Sprintf(apiUrl, upCurrency))
 	if err != nil {
